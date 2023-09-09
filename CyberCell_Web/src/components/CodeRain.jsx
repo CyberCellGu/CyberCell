cae4457c2f29280261a2c6feba1673fb59d93998
M Sami <beboolean@gmail.com>
2023-09-10 02:11:44 +0530

    update

---
+++ CyberCell_Web/src/components/CodeRain.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const CodeRain = () => {
+  const canvasRef = useRef(null);
+  const [fps, setFps] = useState(20);
+  const [color, setColor] = useState('purple');
+  const [charset, setCharset] = useState('0123456789ABCDEF');
+  const [size, setSize] = useState(10);
+
+  const pRef = useRef([]);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    let w, h;
+
+    const resize = () => {
+      w = canvas.width = window.innerWidth;
+      h = canvas.height = window.innerHeight;
+      pRef.current = Array(Math.ceil(w / size)).fill(0);
+    };
+
+    const random = (items) => items[Math.floor(Math.random() * items.length)];
+
+    const draw = () => {
+      ctx.fillStyle = 'rgba(0,0,0,.05)';
+      ctx.fillRect(0, 0, w, h);
+      ctx.fillStyle = color;
+
+      ctx.font = size + 'px sans-serif';
+      for (let i = 0; i < pRef.current.length; i++) {
+        let v = pRef.current[i];
+        ctx.fillText(random(charset), i * size, v);
+        pRef.current[i] = v >= h || v >= 10000 * Math.random() ? 0 : v + size;
+      }
+    };
+
+    const handleResize = () => {
+      resize();
+    };
+
+    window.addEventListener('resize', handleResize);
+    resize();
+
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    intervalRef.current = setInterval(draw, 1000 / fps);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [color, charset, size, fps]);
+
+  const handleFpsChange = (value) => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    setFps(value);
+    intervalRef.current = setInterval(() => draw(), 1000 / value);
+  };
+
+  return (
+      <canvas ref={canvasRef}></canvas>
+      
+  );
+};
+
+export default CodeRain;
--- CyberCell_Web/src/components/Hero.jsx
+++ CyberCell_Web/src/components/Hero.jsx
@@ -1,13 +1,15 @@
 import "./Hero.css";
 import logo from "../assets/cybercelllogoo.png";
 import SphereAnimation from './SphereAnimation';
+import CodeRain from "./CodeRain";
 
 
 function Hero() {
   return (
     <>
-   <div className="hero">
-      <div className="header">
+<CodeRain/>
+   <div className="hero"> 
+      <div className="header">   
         <a href="/">
           <img width={110} src={logo}></img>
         </a>
@@ -41,25 +43,45 @@ function Hero() {
 
   
 
-     <div className="hero-text">
-      <h1>lets hack<br></br><span> Today !</span></h1>
-      </div>
-
+     
+    
+      
+      <div className="box">
+       <h1> Cybercell</h1>
+        <input type="checkbox" name="readmore" id="readmore"/>
+        <div className="content">
+            <div className="des">
+            <span>W</span>elcome to CyberCell GU , where we ignite Cybersecurity Awareness and Innovation. Join us in safeguarding the digital realm through cutting-edge events and initiatives. Unveil the enigma, empower change makers, and collaborate to break barriers.
+           Our tribe of cybersecurity enthusiasts, hackers, and tech visionaries is on a mission to disrupt the disruptors. From immersive workshops to boundary-pushing hackathons, CyberCell is your launchpad for groundbreaking innovation.
+           Our diverse team of dreamers, thinkers, and doers is united by a singular purpose – to make the world better through knowledge. Join us today!
+            </div>
+            <div className="button">
+                <label htmlFor="readmore"
+                    data-more="READ MORE"
+                    data-less="READ LESS"
+                ></label>
+            </div>
+        </div>
+    </div>
 
 
 
   {/* globe start******************************** */}
-  <div id="vanta"></div>
+
  <div  className="globe">
       <div className="feature-animation square-animation"> 
  
     <SphereAnimation/>
 
  </div>
+</div></div>
+
 
 {/* globe end**************************** */}
 
-</div></div>
+
+
+
     </>
  
   );
