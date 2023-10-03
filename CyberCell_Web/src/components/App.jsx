0dd67594587316c3fb1902d96db9b1c35f07418b
vishnu suthar <sutharvishnu2021@gmail.com>
2023-10-03 22:21:24 +0530

    react-router work done

--- CyberCell_Web/src/components/App.jsx
+++ CyberCell_Web/src/components/App.jsx
@@ -1,22 +1,26 @@
 import "./App.css";
 import Hero from "./Hero";
 import Games from "./Games";
-import React from 'react';
-import Members from "./Members/Members.jsx"
+import React from "react";
+import Members from "./Members/Members.jsx";
+import Login from "./Login/Login";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Header/header";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" Component={Hero}/>
-        
-        <Route path="/games"  Component={Games}/>
-       
-       
+        <Route path="/" Component={Hero} />
+        <Route path="/games" Component={Games} />
+        <Route path="/login" Component={Login} />
+        <Route path="/hacktivists" Component={Members} />
       </Routes>
-    </Router>  
+      <Footer />
+    </Router>
   );
 }
 
---
+++ CyberCell_Web/src/components/Header/header.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import logo from "../../assets/cybercelllogoo.png";
+import icon from "../../assets/hacker.png";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+// import logo from "../assets/cybercelllogoo.png";
+const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <div className="header">
+      <a href="/">
+        <img width={110} src={logo} alt="CyberCell Logo" />
+      </a>
+      <div className={`navbar ${navbarOpen ? "active" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <HashLink to="/#games">Events</HashLink>
+          </li>
+          <li>
+            {" "}
+            <Link to="/games">Games</Link>
+          </li>
+          <li>
+            <Link to="hacktivists">Hacktivists</Link>
+          </li>
+          <li>
+            <HashLink>About</HashLink>
+          </li>
+
+          <button>
+            <Link to="/login">LOGIN</Link>
+          </button>
+
+          <img
+            style={{ cursor: "pointer" }}
+            width={35}
+            src={icon}
+            alt="Hacker Icon"
+            onClick={() => {
+              updateColor("green");
+            }}
+          />
+        </ul>
+      </div>
+      <div id="mobile" onClick={() => setNavbarOpen((prev) => !prev)}>
+        <i id="bar" className={navbarOpen ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+    </div>
+  );
+};
+export default Header;
